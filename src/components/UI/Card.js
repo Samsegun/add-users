4a6110795b15dbeb151cb7e876dd8e4d35a740ce
Samsegun <oyebadesamsegun@gmail.com>
2022-01-21 12:44:36 +0100

    comments

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,6 +1,7 @@
 import styles from "./Card.module.css";
 
 const Card = props => {
+  // add extra classes where necessary
   const classes = `${styles.card} ${props.className ? props.className : ""}`;
 
   return <div className={classes}>{props.children}</div>;
